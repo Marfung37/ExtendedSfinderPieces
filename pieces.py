4d951029fb276efcb7b1d4122c4a6781eaf44a20
Marvin Fung <marvin.fung37@gmail.com>
2026-06-27 22:50:32 -0500

    Updates pieces.py file to be a basic cmdline util

--- pieces.py
+++ pieces.py
@@ -1,1 +1,12 @@
-from sfinder_pieces import sfinder_pieces, random_sfinder_pieces
+from sfinder_pieces import sfinder_pieces
+from sys import argv
+
+if __name__ == "__main__":
+  if len(argv) != 2:
+    print("Usage: python pieces.py [pattern]")
+    exit()
+  # print out all queues for passed in string
+  pattern = argv[1]
+
+  for queue in sfinder_pieces(pattern):
+    print(queue)
