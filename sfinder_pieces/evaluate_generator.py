22146ad6da9a0685336d13ddd0f8c9aef202881b
Marvin Fung <marvin.fung37@gmail.com>
2026-07-14 23:54:09 -0500

    Revert "Adds slight optimization to avoid unnecessary sorting in generation"
    This reverts commit cccc6bfc5dc7214c0e81c1c5e676dde6cb63241b.

--- sfinder_pieces/evaluate_generator.py
+++ sfinder_pieces/evaluate_generator.py
@@ -1,5 +1,6 @@
 from itertools import product, permutations, chain
 from .parser import GeneratorLiteral
+from .utils import tetris_order_key
 from math import perm
 import random
 
@@ -19,7 +20,7 @@ def evaluate_generator(node: GeneratorLiteral) -> list[str]:
   )
 
   # sort and remove duplicates
-  return list(dict.fromkeys(queues))
+  return sorted(set(queues), key=tetris_order_key)
 
 
 def sampleable_generator(node: GeneratorLiteral) -> bool:
